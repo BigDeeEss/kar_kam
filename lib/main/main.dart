b5384bb8e5a7c9878c6f082b14be9150e0bba785
Damon Swatton on INSPIRON <damon.swatton@gmail.com>
2023-06-18 10:40:40 +0100

    Rename change -> update in AppData and AppDataManagerMixin.
    Remove force input parameter from update and update comments.

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -12,7 +12,7 @@ void main() {
   // register an instance of [AppDataPreferenceService].
   //
   // The constructor for [AppDataPreferenceService] loads preferences from
-  // file and/or apply defaults.
+  // file and/or applies defaults.
   GetItService.register<AppData>(AppDataPreferenceService());
 
   // Run the app.
