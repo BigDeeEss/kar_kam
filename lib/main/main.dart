c7160e97357430f1352c2c501c540e9f657a2e7a
Damon Swatton on INSPIRON <damon.swatton@gmail.com>
2023-06-19 21:52:11 +0100

    Complete implementation of AppDataPreferenceService.
    AppDataPreferenceService adds the ability to be able to store user prefs on storage devices so that it can be retrieved by Kar Kam app after restarts.

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -16,23 +16,5 @@ void main() {
   GetItService.register<AppData>(AppDataPreferenceService());
 
   // Run the app.
-  runApp(const MyApp());
+  runApp(KarKam());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Kar Kam',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Placeholder(),
-    );
-  }
-}
-
