BigDeeEss/kar_kam_1

Add BaseUILayout, generate home instance of BaseUILayout.
Also add string_extension.dart.

lib/base_ui/base_ui.dart
@@ -2,8 +2,10 @@
 import 'package:flutter/material.dart';
 
 // Import project-specific files.
+import 'package:kar_kam_1/base_ui/base_ui_layout.dart';
 import 'package:kar_kam_1/base_ui/base_ui_view.dart';
 import 'package:kar_kam_1/utils/data_store.dart';
+import 'package:kar_kam_1/utils/string_extension.dart';
 
 /// Implements a generic page layout design.
 ///
@@ -18,11 +20,11 @@ import 'package:kar_kam_1/utils/data_store.dart';
 class BaseUI extends StatelessWidget {
   const BaseUI({
     super.key,
-    // this.baseUISpec,
+    this.baseUILayout,
   });
 
   /// Defines the current layout of the UI..
-  // final BaseUISpec? baseUISpec;
+  final BaseUILayout? baseUILayout;
 
   @override
   Widget build(BuildContext context) {
@@ -33,13 +35,14 @@ class BaseUI extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: baseUILayout?.title.toText,
       ),
       body: DataStore<GlobalKey>(
         key: const ValueKey('baseUIViewKey'),
         data: baseUIViewKey,
         child: BaseUIView(
           key: baseUIViewKey,
-          children: const [Placeholder()],
+          baseUILayout: baseUILayout,
           // children: baseUISpec,
         ),
       ),

lib/main.dart
@@ -7,6 +7,7 @@ import 'package:kar_kam_1/app_data/app_data.dart';
 import 'package:kar_kam_1/app_data/app_data_preferences_service.dart';
 import 'package:kar_kam_1/app_data/get_it_service.dart';
 import 'package:kar_kam_1/base_ui/base_ui.dart';
+import 'package:kar_kam_1/home/home.dart';
 
 /// App start point.
 void main() {
@@ -54,7 +55,7 @@ class KarKam extends StatelessWidget with GetItMixin{
           if (snapshot.hasData) {
             // For the 'has data' case, when the load of app settings
             // is complete, continue with building BasePage.
-            return const BaseUI();
+            return BaseUI(baseUILayout: home);
           } else {
             // For the 'has no data' case, where the load of app settings
             // is still in progress, present a progress indicator.

BigDeeEss/kar_kam

lib/base_ui/base_ui.dart
@@ -2,8 +2,10 @@
 import 'package:flutter/material.dart';
 
 // Import project-specific files.
+import 'package:kar_kam/base_ui/base_ui_layout.dart';
 import 'package:kar_kam/base_ui/base_ui_view.dart';
 import 'package:kar_kam/utils/data_store.dart';
+import 'package:kar_kam/utils/string_extension.dart';
 
 /// Implements a generic page layout design.
 ///
@@ -18,11 +20,11 @@ import 'package:kar_kam/utils/data_store.dart';
 class BaseUI extends StatelessWidget {
   const BaseUI({
     super.key,
-    // this.baseUISpec,
+    this.baseUILayout,
   });
 
   /// Defines the current layout of the UI..
-  // final BaseUISpec? baseUISpec;
+  final BaseUILayout? baseUILayout;
 
   @override
   Widget build(BuildContext context) {
@@ -33,13 +35,14 @@ class BaseUI extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: baseUILayout?.title.toText,
       ),
       body: DataStore<GlobalKey>(
         key: const ValueKey('baseUIViewKey'),
         data: baseUIViewKey,
         child: BaseUIView(
           key: baseUIViewKey,
-          children: const [Placeholder()],
+          baseUILayout: baseUILayout,
           // children: baseUISpec,
         ),
       ),

lib/main.dart
@@ -7,6 +7,7 @@ import 'package:kar_kam/app_data/app_data.dart';
 import 'package:kar_kam/app_data/app_data_preferences_service.dart';
 import 'package:kar_kam/app_data/get_it_service.dart';
 import 'package:kar_kam/base_ui/base_ui.dart';
+import 'package:kar_kam/home/home.dart';
 
 /// App start point.
 void main() {
@@ -54,7 +55,7 @@ class KarKam extends StatelessWidget with GetItMixin{
           if (snapshot.hasData) {
             // For the 'has data' case, when the load of app settings
             // is complete, continue with building BasePage.
-            return const BaseUI();
+            return BaseUI(baseUILayout: home);
           } else {
             // For the 'has no data' case, where the load of app settings
             // is still in progress, present a progress indicator.