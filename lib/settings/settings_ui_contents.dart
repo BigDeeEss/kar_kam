cc9e8e0b5abd7c18115a9de709bedf463a83e9d5
Damon Swatton on INSPIRON <damon.swatton@gmail.com>
2023-07-04 20:11:23 +0100

    Add _SettingsUIContents, a new StatefulWidget in settings_ui_contents.dart.

--- lib/settings/settings_ui_contents.dart
+++ lib/settings/settings_ui_contents.dart
@@ -7,6 +7,21 @@ import 'package:kar_kam/base_ui/base_ui_contents.dart';
 /// Home layout.
 BaseUIContents settingsUIContents = BaseUIContents(
   title: 'Settings',
-  contents: Container(),
+  contents: _SettingsUIContents(),
   floatingActionButtonTargetList: <String>['karKamUIContents'],
 );
+
+
+class _SettingsUIContents extends StatefulWidget {
+  const _SettingsUIContents({super.key});
+
+  @override
+  State<_SettingsUIContents> createState() => _SettingsUIContentsState();
+}
+
+class _SettingsUIContentsState extends State<_SettingsUIContents> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
